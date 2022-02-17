e05d20a7e440c5c7d1956245d4ac2a33ae03dbe2
MrGhost027 <95536015+MrGhost027@users.noreply.github.com>
2022-02-16 21:03:53 -0300

    Create GhostFa.bat

---
+++ GhostFa.bat
@@ -0,0 +1,23 @@
+@echo off
+slmgr /xpr
+
+color 2
+
+start https://pbs.twimg.com/media/DJaMH7EWAAAucDN.jpg
+start https://pbs.twimg.com/media/DJaMH7EWAAAucDN.jpg
+systeminfo
+echo ************************************************************************************************************************
+ipconfig
+
+:loop
+title Indio Ana Jones
+title Poetico
+title Aphonsiano System
+title Toda vez
+title Marcio corno
+title Bando de $#@#@!
+title Pedro Henrique + Marcio
+title Marcio -5684 = corno
+goto loop
+
+pause
